43b4ad7056d05d4c8689dc4924e215371ac4e8c8
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-22 20:33:10 +0900

    SwiftLintを導入 (#1)

--- iOSEngineerCodeCheck/SearchListViewController.swift
+++ iOSEngineerCodeCheck/SearchListViewController.swift
@@ -9,54 +9,47 @@
 import UIKit
 
 class SearchListViewController: UITableViewController {
-
     @IBOutlet private weak var searchBar: UISearchBar!
-    
-    var repo: [[String: Any]]=[]
-    
+
+    var repo: [[String: Any]] = []
+
     var task: URLSessionTask?
     var word: String!
     var url: String!
     var idx: Int!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         searchBar.text = "GitHubのリポジトリを検索できるよー"
         searchBar.delegate = self
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == "Detail"{
-            let dtl = segue.destination as! DetailViewController
-            dtl.vc1 = self
+            let dtl = segue.destination as? DetailViewController
+            dtl?.vc1 = self
         }
-        
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return repo.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = UITableViewCell()
-        let rp = repo[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let repo = repo[indexPath.row]
+        cell.textLabel?.text = repo["full_name"] as? String ?? ""
+        cell.detailTextLabel?.text = repo["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
-        
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
         idx = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
-        
     }
-    
 }
 
 // MARK: - UISearchBarDelegate
@@ -66,30 +59,28 @@ extension SearchListViewController: UISearchBarDelegate {
         searchBar.text = ""
         return true
     }
-    
+
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         task?.cancel()
     }
-    
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
         word = searchBar.text!
-        
-        if word.count != 0 {
+
+        if !word.isEmpty {
             url = "https://api.github.com/search/repositories?q=\(word!)"
-            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
-                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
+            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, _, _) in
+                if let obj = try? JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                     if let items = obj["items"] as? [[String: Any]] {
-                    self.repo = items
+                        self.repo = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
                         }
                     }
                 }
             }
-        // これ呼ばなきゃリストが更新されません
-        task?.resume()
+            // これ呼ばなきゃリストが更新されません
+            task?.resume()
         }
-        
     }
 }
