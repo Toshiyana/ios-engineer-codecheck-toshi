9723ec7bc03b822ddd287f7f494baee850268582
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-23 23:32:46 +0900

    Moyaを用いた通信のためのGitHubTargetを追加

---
+++ iOSEngineerCodeCheck/Moya/GitHubTarget.swift
@@ -0,0 +1,62 @@
+//
+//  GitHubTarget.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Toshiyana on 2022/07/23.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Moya
+
+enum GitHubTarget {
+    case searchRepository(keyValue: [String: Any])
+}
+
+extension GitHubTarget: TargetType {
+    var baseURL: URL {
+        return URL(string: "https://api.github.com/")!
+    }
+    
+    var path: String {
+        switch self {
+        case .searchRepository:
+            return "search/repositories"
+        }
+    }
+    
+    var parameters: [String: Any] {
+        // GitHubAPIのDefaultの設定
+        var parameter = [
+            "order": "desc",
+            "per_page": 30,
+            "page": 1
+        ] as [String: Any]
+        
+        switch self {
+        case .searchRepository(let keyValue):
+            keyValue.forEach({ key, value in
+                parameter[key] = value
+            })
+            return parameter
+        }
+    }
+    
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        case .searchRepository:
+            return Moya.URLEncoding.queryString
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .searchRepository:
+            print("DEBUG: parameters:: \(parameters)")
+            return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
