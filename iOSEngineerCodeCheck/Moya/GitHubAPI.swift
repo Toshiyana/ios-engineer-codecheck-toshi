9ff0329322a609d57a1cb43ce7d88cb1a5e6b774
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-24 01:45:48 +0900

    MoyaErrorを除去して、GitHubAPIErrorを定義

--- iOSEngineerCodeCheck/Moya/GitHubAPI.swift
+++ iOSEngineerCodeCheck/Moya/GitHubAPI.swift
@@ -9,33 +9,39 @@
 import Foundation
 import Moya
 
-typealias CompletionHandler<T> = (Result<T, MoyaError>) -> Void
+typealias CompletionHandler<T> = (Result<T, GitHubAPIError>) -> Void
 
 final class GitHubAPI {
+    static let shared = GitHubAPI()
+
     // providerでNetworkLoggerPlugin()を指定し、リクエストのログを表示
     private let provider = MoyaProvider<GitHubTarget>(plugins: [NetworkLoggerPlugin()])
-    
+
     func searchRepository(keyValue: [String: Any], completion: @escaping CompletionHandler<GitHubResponse>) {
         provider.request(.searchRepository(keyValue: keyValue)) { [weak self] result in
             guard let strongSelf = self else { return }
             switch result {
             case .success(let response):
                 completion(strongSelf.decodeToGitHubResponse(response: response))
-            case .failure(let moyaError):
-                completion(.failure(moyaError))
+            case .failure:
+                completion(.failure(.responseError))
             }
         }
     }
-    
-    private func decodeToGitHubResponse(response: Response) -> Result<GitHubResponse, MoyaError> {
+
+    private func decodeToGitHubResponse(response: Response) -> Result<GitHubResponse, GitHubAPIError> {
         do {
             let decoder = JSONDecoder()
             let response = try response.filterSuccessfulStatusAndRedirectCodes()
             let gitHubResponse = try response.map(GitHubResponse.self, using: decoder)
             return .success(gitHubResponse)
-        } catch let error {
-            let moyaError = error as! MoyaError
-            return .failure(moyaError)
+        } catch {
+            return .failure(.decodeError)
         }
     }
+}
+
+enum GitHubAPIError: Error {
+    case decodeError
+    case responseError
 }
