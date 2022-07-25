6add44146ed64e92dbd95c4e017881ff7bcf586f
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-25 20:39:39 +0900

    searchRepository()の戻り値をObservableに変更

---
+++ iOSEngineerCodeCheck/Model/Error/APIResponseStatusCodeHandler.swift
@@ -0,0 +1,9 @@
+//
+//  APIResponseStatusCodeHandler.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Toshiyana on 2022/07/25.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
--- iOSEngineerCodeCheck/Model/Moya/GitHubAPI.swift
+++ iOSEngineerCodeCheck/Model/Moya/GitHubAPI.swift
@@ -10,37 +10,15 @@ import RxSwift
 import Moya
 
 final class GitHubAPI {
-    private let provider = MoyaProvider<GitHubTarget>()
+    private static let provider = MoyaProvider<GitHubTarget>()
     
-    func searchRepository(keyValue: [String: Any]) throws -> Single<GitHubResponse> {
-        return Single<GitHubResponse>.create { [provider] observer in
-            provider.request(
-                .searchRepository(keyValue: keyValue)) { [weak self] response in
-                    guard let strongSelf = self else { return }
-                    switch response {
-                    case .success(let response):
-                        observer(strongSelf.decodeToGitHubResponse(response: response))
-                    case .failure:
-                        observer(.failure(GitHubAPIError.responseError))
-                    }
+    static func searchRepository(keyValue: [String: Any]) throws -> Observable<GitHubResponse> {
+        return provider.rx.request(
+            .searchRepository(keyValue: keyValue))
+            .map { response in
+                try APIResponseStatusCodeHandler.handleStatusCode(response)
+                return try JSONDecoder().decode(GitHubResponse.self, from: response.data)
             }
-            return Disposables.create()
-        }
+            .asObservable()
     }
-    
-    private func decodeToGitHubResponse(response: Response) -> Result<GitHubResponse, Error> {
-        do {
-            let decoder = JSONDecoder()
-            let response = try response.filterSuccessfulStatusAndRedirectCodes()
-            let gitHubResponse = try response.map(GitHubResponse.self, using: decoder)
-            return .success(gitHubResponse)
-        } catch {
-            return .failure(GitHubAPIError.decodeError)
-        }
-    }
-}
-
-enum GitHubAPIError: Error {
-    case decodeError
-    case responseError
 }
