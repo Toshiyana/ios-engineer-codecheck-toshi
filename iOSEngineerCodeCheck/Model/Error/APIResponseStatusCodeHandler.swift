99f343f9c9084f351baf53f88035fe39c1a2a20a
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-25 20:40:46 +0900

    APIResponseのStatusCodeHandlerを追加

--- iOSEngineerCodeCheck/Model/Error/APIResponseStatusCodeHandler.swift
+++ iOSEngineerCodeCheck/Model/Error/APIResponseStatusCodeHandler.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2022 YUMEMI Inc. All rights reserved.
 //
 
-import Foundation
+import Moya
+import RxSwift
+
+enum APIError: Error {
+    case unexpectedServerError
+    var errorTitle: String? {
+        switch self {
+        case .unexpectedServerError:
+            return "予期せぬサーバーエラーが発生しました"
+        }
+    }
+}
+
+final class APIResponseStatusCodeHandler {
+    // ステータスコードハンドリング共通メソッド
+    static func handleStatusCode(_ response: PrimitiveSequence<SingleTrait, Response>.Element) throws {
+        print("DEBUG: handleStatusCode:: \(response.statusCode)")
+        switch response.statusCode {
+        case 200...399:
+            break
+        default:
+            throw APIError.unexpectedServerError
+        }
+    }
+}
