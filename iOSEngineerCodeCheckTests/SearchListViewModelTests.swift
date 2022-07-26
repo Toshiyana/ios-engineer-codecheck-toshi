df7bc8c88a5490134aa0b38e416e7410bfc2c1ac
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-26 22:03:04 +0900

    インジケータ表示のため、SPMでPKHUDを導入 (#7)

--- iOSEngineerCodeCheckTests/SearchListViewModelTests.swift
+++ iOSEngineerCodeCheckTests/SearchListViewModelTests.swift
@@ -1,15 +1,14 @@
 //
-//  iOSEngineerCodeCheckTests.swift
+//  SearchListViewModelTests.swift
 //  iOSEngineerCodeCheckTests
 //
-//  Created by 史 翔新 on 2020/04/20.
-//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//  Created by Toshiyana on 2022/07/26.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
 //
 
 import XCTest
-@testable import iOSEngineerCodeCheck
 
-class iOSEngineerCodeCheckTests: XCTestCase {
+class SearchListViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,6 +21,9 @@ class iOSEngineerCodeCheckTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
