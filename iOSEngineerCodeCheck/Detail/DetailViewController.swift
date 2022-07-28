d4dce5719b8fe509d6a49bc3de7f9aed7195ad6f
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-29 04:31:02 +0900

    DetailVCを修正 (#7)

--- iOSEngineerCodeCheck/Detail/DetailViewController.swift
+++ iOSEngineerCodeCheck/Detail/DetailViewController.swift
@@ -12,33 +12,80 @@ import Kingfisher
 final class DetailViewController: UIViewController {
     @IBOutlet private weak var iconImageView: UIImageView!
     @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var languageLabel: UILabel!
-    @IBOutlet private weak var starsCountLabel: UILabel!
-    @IBOutlet private weak var watchersCountLabel: UILabel!
-    @IBOutlet private weak var forksCountLabel: UILabel!
-    @IBOutlet private weak var openIssuesCountLabel: UILabel!
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var favoriteButton: UIButton!
+    @IBOutlet private weak var accessPageButton: UIButton!
+
+    @IBOutlet private weak var tableViewHeightConstant: NSLayoutConstraint! // ScrollView内におけるTableViewのHeightを自動調整するための変数
 
     var repoItem: RepoItem?
+    private let infoName = ["Language", "Stars Count", "Watchers Count", "Forks Count",
+                            "Open Issues Count"]
+    private var infoValue: [String]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.numberOfLines = 1
-        titleLabel.minimumScaleFactor = 0.5
-        titleLabel.adjustsFontSizeToFitWidth = true
+        if let repoItem = repoItem {
+            infoValue = [repoItem.language ?? "", "\(repoItem.stargazersCount)",
+                         "\(repoItem.watchersCount)", "\(repoItem.forksCount)",
+                         "\(repoItem.openIssuesCount)"]
+        }
 
         setupUI()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
+        tableView.reloadData()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tableView.removeObserver(self, forKeyPath: "contentSize")
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "contentSize" {
+            if let newValue = change?[.newKey] {
+                let newSize = newValue as! CGSize
+                tableViewHeightConstant.constant = newSize.height
+            }
+        }
     }
 
     private func setupUI() {
         if let repoItem = repoItem {
             titleLabel.text = repoItem.fullName
-            languageLabel.text = "Written in \(repoItem.language ?? "")"
-            starsCountLabel.text = "\(repoItem.stargazersCount) stars"
-            watchersCountLabel.text = "\(repoItem.watchersCount) watchers"
-            forksCountLabel.text = "\(repoItem.forksCount) forks"
-            openIssuesCountLabel.text = "\(repoItem.openIssuesCount) open issues"
             iconImageView.kf.setImage(with: repoItem.owner.avatarUrl)
         }
+
+        tableView.register(RepoInformationCell.nib(), forCellReuseIdentifier: RepoInformationCell.identifier)
+        // 文字サイズを自動調整
+        titleLabel.numberOfLines = 1
+        titleLabel.minimumScaleFactor = 0.5
+        titleLabel.adjustsFontSizeToFitWidth = true
+    }
+}
+
+extension DetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return infoName.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RepoInformationCell.identifier, for: indexPath) as! RepoInformationCell
+
+        guard let infoValue = infoValue else { return UITableViewCell() }
+        cell.configure(infoName: infoName[indexPath.row],
+                       infoValue: infoValue[indexPath.row])
+        return cell
+    }
+}
+
+extension DetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 }
