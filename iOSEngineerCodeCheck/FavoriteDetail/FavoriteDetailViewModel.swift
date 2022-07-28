9511a289472c6ed67473a09edbef1946f67df437
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-29 07:41:12 +0900

    FavoriteDetailViewModelを追加 (#8)

--- iOSEngineerCodeCheck/FavoriteDetail/FavoriteDetailViewModel.swift
+++ iOSEngineerCodeCheck/FavoriteDetail/FavoriteDetailViewModel.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol FavoriteDetailViewModelInputs {
+    var removeFavorite: PublishRelay<String> { get }
+    var getFavoriteCondition: PublishRelay<String> { get }
+}
+
+protocol FavoriteDetailViewModelOutputs {
+    var activeFavorite: PublishRelay<Bool> { get }
+}
+
+protocol FavoriteDetailViewModelType {
+    var inputs: FavoriteDetailViewModelInputs { get }
+    var outputs: FavoriteDetailViewModelOutputs { get }
+}
+
+final class FavoriteDetailViewModel: FavoriteDetailViewModelInputs, FavoriteDetailViewModelOutputs {
+    // MARK: - FavoriteDetailViewModelInputs
+    var removeFavorite = PublishRelay<String>()
+    var getFavoriteCondition = PublishRelay<String>()
+
+    // MARK: - FavoriteDetailViewModelOutputs
+    var activeFavorite = PublishRelay<Bool>()
+
+    private let disposeBag = DisposeBag()
+
+    init() {
+        removeFavorite
+            .subscribe(onNext: { id in
+                RealmManager.deleteOneObject(type: RepoItemObject.self, id: id)
+            })
+            .disposed(by: disposeBag)
+
+        getFavoriteCondition
+            .subscribe(onNext: { [weak self] id in
+                guard let strongSelf = self,
+                      let repoItemObjects = RealmManager.getEntityList(type: RepoItemObject.self)
+                else { return }
+
+                for i in 0 ..< repoItemObjects.count {
+                    if repoItemObjects[i].id == id {
+                        strongSelf.activeFavorite.accept(true)
+                        return
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - FavoriteDetailViewModelType
+extension FavoriteDetailViewModel: FavoriteDetailViewModelType {
+    var inputs: FavoriteDetailViewModelInputs { return self }
+    var outputs: FavoriteDetailViewModelOutputs { return self }
+}
