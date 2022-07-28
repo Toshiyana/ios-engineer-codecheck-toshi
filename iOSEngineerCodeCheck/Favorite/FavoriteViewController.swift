ff98dab97fde43e794bb1ef484426e770271f603
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-29 01:39:09 +0900

    お気に入り画面のUIを追加 (#8)

---
+++ iOSEngineerCodeCheck/Favorite/FavoriteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Toshiyana on 2022/07/29.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
