b9ce19d1bd4f5e416839a053107b99113d6230a1
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-28 18:09:52 +0900

    SearchListViewModelのUnitTestを追加 (#9)

--- iOSEngineerCodeCheckTests/SearchListViewModelTests.swift
+++ iOSEngineerCodeCheckTests/SearchListViewModelTests.swift
@@ -10,28 +10,29 @@
 import XCTest
 
 class SearchListViewModelTests: XCTestCase {
+    var mockGitHubAPI: MockGitHubAPI!
+    var sut: SearchListViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mockGitHubAPI = MockGitHubAPI()
+        sut = SearchListViewModel(githubAPI: mockGitHubAPI)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockGitHubAPI = nil
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testSearchListViewModel_WhenSearchButtonClicked_Success() {
+        sut.inputs.searchBarText.accept(String.mock)
+        sut.inputs.searchButtonClicked.accept(())
+        XCTAssertEqual(sut.outputs.repoItems.value, MockData.getRepoItems(), "期待する検索結果が返ってくること")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSearchListViewModel_WhenSearchButtonClicked_Failure() {
+        mockGitHubAPI.shouldReturnError = true
+        sut.inputs.searchBarText.accept(String.mock)
+        sut.inputs.searchButtonClicked.accept(())
+        // TODO: Error発生を確認（Assertの方法が分からなかった）
     }
-
 }
