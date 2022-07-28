285a9f3ba694351d183717124cb9a22fcbc4dc64
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-29 05:10:41 +0900

    FavoriteVCを追加

--- iOSEngineerCodeCheck/Favorite/FavoriteViewController.swift
+++ iOSEngineerCodeCheck/Favorite/FavoriteViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 final class FavoriteViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
