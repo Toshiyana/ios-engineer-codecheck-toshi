b4b350574327ff37414fc8e739c73440f9e3d690
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-28 23:11:37 +0900

    SwiftLintでfunction_body_length, cyclomatic_complexityルールを除去

---
+++ iOSEngineerCodeCheck/Model/Manager/QueryShareManager.swift
@@ -0,0 +1,9 @@
+//
+//  QueryShareManager.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Toshiyana on 2022/07/28.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
