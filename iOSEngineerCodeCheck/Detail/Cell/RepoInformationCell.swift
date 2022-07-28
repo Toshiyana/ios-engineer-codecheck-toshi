0c2444c9412728be239b52c0589c1e37d75757c6
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-29 04:28:50 +0900

    DetailVCのUIをブラッシュアップ (#7)

---
+++ iOSEngineerCodeCheck/Detail/Cell/RepoInformationCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoInformationCell.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Toshiyana on 2022/07/29.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class RepoInformationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
